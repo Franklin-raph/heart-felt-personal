7286d6c4b20b24ba8c263c16bf04a82d02c9e066
Okoro James <okorojameschizaram@gmail.com>
2023-09-02 14:41:14 +0100

    work on the single card view sign card modal

--- src/components/alert/ErrorAlert.jsx
+++ src/components/alert/ErrorAlert.jsx
@@ -1,26 +1,48 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 // , fromEmailVerify, setFromEmailVerify
-const ErrorAlert = ({error, setError}) => {
-    console.log(error)
-    const navigate = useNavigate()
+const ErrorAlert = ({ error, setError }) => {
+  console.log(error);
+  const navigate = useNavigate();
   return (
     <div>
-        <div className="successModalBg">
-            <div className="failureModal flex items-center justify-center flex-col gap-10" style={{ position:"relative" }}>
-                <i class="fa-solid fa-xmark" style={{ color:"#333", position:"absolute", cursor:"pointer", top:"15px", right:"15px", fontSize:"22px" }} onClick={() => setError(false)}></i>
-                <svg class="cancel" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                    <path class="check__cancel" fill="none" d="M14.1 14.1l23.8 23.8 m0,-23.8 l-23.8,23.8" />
-                </svg>
-                <p style={{ color:"black" }}>{error}</p>
-                {/* {fromEmailVerify && <button onClick={()=> {
+      <div className="successModalBg">
+        <div
+          className="failureModal flex items-center justify-center flex-col gap-10"
+          style={{ position: "relative" }}
+        >
+          <i
+            className="fa-solid fa-xmark"
+            style={{
+              color: "#333",
+              position: "absolute",
+              cursor: "pointer",
+              top: "15px",
+              right: "15px",
+              fontSize: "22px",
+            }}
+            onClick={() => setError(false)}
+          ></i>
+          <svg
+            className="cancel"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 52 52"
+          >
+            <path
+              className="check__cancel"
+              fill="none"
+              d="M14.1 14.1l23.8 23.8 m0,-23.8 l-23.8,23.8"
+            />
+          </svg>
+          <p style={{ color: "black" }}>{error}</p>
+          {/* {fromEmailVerify && <button onClick={()=> {
                     navigate("/login")
                     setFromEmailVerify(false)
                 }}>Continue to login</button>} */}
-            </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorAlert
+export default ErrorAlert;
