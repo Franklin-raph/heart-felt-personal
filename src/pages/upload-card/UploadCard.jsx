6a2a9a823e0f583bbab8258a1fed4ae0f3b46a0f
franklin-raph <igboekwulusifranklin@gmail.com>
2023-09-18 21:01:47 +0100

    implemented the card upload funtionality

--- src/pages/upload-card/UploadCard.jsx
+++ src/pages/upload-card/UploadCard.jsx
@@ -4,41 +4,52 @@ import { useNavigate } from "react-router-dom";
 
 const UploadCard = ({baseUrl}) => {
   const navigate = useNavigate();
-  const [imgFile, setImgFile] = useState(null)
-  console.log(baseUrl)
+  const [imgFile, setImgFile] = useState()
+  const [isChecked, setIsChecked] = useState(false)
+  const [loader, setLoader] = useState(false)
+  const [error, setError] = useState()
+  const [success, setSuccess] = useState()
   const user = JSON.parse(localStorage.getItem("user"));
-  console.log(user)
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
 
   //
-  const getUploadFile = async (e) => {
-    // const file = e.target.files[0];
-    // setImgFile(e.target.files[0])
-
-    // console.log(file)
-    console.log(imgFile)
-
-    // let reader = new FileReader();
-
-    // reader.onload = (e) => {
-    //   const fileImg = e.target.result;
-    //   localStorage.setItem("uploaded_card_img", fileImg);
-    //   navigate("/preview-uploaded-card");
-    // };
-    // reader.readAsDataURL(file);
-
-    const fd = new FormData();
-    fd.append('image', imgFile)
-    const response = await fetch(`${baseUrl}/upload`,{
-      method:"POST",
-      body: fd,
-      headers:{
-        Authorization: `Bearer ${user.accessToken}`
-      }
-    })
-    const data = await response.json()
-    console.log(data)
+  const getUploadFile = (e) => {
+    if(e.target.files && e.target.files.length > 0){
+      setImgFile(e.target.files[0])
+    }
+    console.log(URL.createObjectURL(e.target.files[0]))
 
   };
+
+  async function uploadImageToServer(){
+    if(isChecked){
+      setLoader(true)
+      const fd = new FormData();
+      fd.append('image', imgFile)
+      console.log(imgFile)
+      const response = await fetch(`${baseUrl}/upload`,{
+        method:"POST",
+        body: fd,
+        headers:{
+          Authorization: `Bearer ${user.accessToken}`
+        }
+      })
+      const data = await response.json()
+      if(response) setLoader(false)
+      if(response.ok){
+        alert(data.message)
+      }else{
+        console.log(data.message)
+      }
+      console.log(data)
+    }else{
+      navigate("/preview-uploaded-card")
+    }
+
+  }
 
   //
   return (
@@ -52,46 +63,84 @@ const UploadCard = ({baseUrl}) => {
             <p onClick={() => navigate("/saved-card")}>Saved Covers</p>
           </div>
         </div>
-        <div className="body">
-          <label
-            className="first_upload_card_label"
-            htmlFor="upload_card_input"
-          >
-            <i className="bx bx-images"></i>
-          </label>
-          <input type="file" id="upload_card_input" onChange={(e) => setImgFile(e.target.files[0])} />
-          <p>
-            Please upload and image or,{" "}
-            <label
-              className="second_upload_card_label"
-              htmlFor="upload_card_input"
-            >
-              Browse
-            </label>{" "}
-          </p>
-          <p>Maximum 5mb file size. 440px width and 550px height recommended</p>
-          <p>
-            <span></span>Animated Gifs
-          </p>
-          <p>
-            <span></span>Files should contain 50 frames or less
-          </p>
-        </div>
+        
+        {!imgFile &&
+          <>
+            <div className="body">
+              <label
+                className="first_upload_card_label"
+                htmlFor="upload_card_input"
+              >
+                <i className="bx bx-images"></i>
+              </label>
+              <input type="file" id="upload_card_input" onChange={e => getUploadFile(e)} />
+              <p>
+                Please upload and image or,
+                <label
+                  className="second_upload_card_label"
+                  htmlFor="upload_card_input"
+                >
+                  Browse
+                </label>
+              </p>
+              <p>Maximum 5mb file size. 440px width and 550px height recommended</p>
+              <p>
+                <span></span>Animated Gifs
+              </p>
+              <p>
+                <span></span>Files should contain 50 frames or less
+              </p>
+            </div>
+          </>
+        }
+        
       </div>
-      <div className="upload-card-nav flex-between g-3">
-        <button onClick={() => navigate("/upload-card-cover-birthday")}>
-          Cancel
-        </button>
-        <div className="edit_and_continue">
-          <button
-            className="primary-button"
-            // onClick={() => navigate("/preview-uploaded-card")}
-            onClick={getUploadFile}
-          >
-            Continue
-          </button>
+      
+
+      {imgFile && 
+        <div style={{ textAlign:"center" }}>
+          <img src={URL.createObjectURL(imgFile)} alt="Current image" className="uploaded_img" style={{ margin:"3rem auto 10px" }}/>
+            <div className="upload-card-nav flex-between g-3">
+              <button onClick={() => navigate("/upload-card-cover-birthday")}>
+                Cancel
+              </button>
+              <div className="flex-center">
+                <input type="checkbox" 
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+                />
+                <p
+                  style={{
+                    color: "var(--body-texts)",
+                    fontSize: "13px",
+                    margin: "3px 0 0 5px",
+                  }}
+                >
+                  Save my card cover
+                </p>
+              </div>
+              {!loader ?
+              <div className="edit_and_continue">
+                <button
+                  className="primary-button"
+                  onClick={uploadImageToServer}
+                >
+                  Continue
+                </button>
+              </div>
+               :
+               <div className="edit_and_continue">
+                <button
+                    className="primary-button"
+                  >
+                    <i className="fa-solid fa-spinner fa-spin"></i>
+                  </button>
+                </div>
+                }
+              
+            </div>
         </div>
-      </div>
+      }
     </div>
   );
 };
